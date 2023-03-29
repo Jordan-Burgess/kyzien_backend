6b1f7ed1bf2b854abee5b7fd496ad3d9c1e871f4
Jordan-Burgess <alfredjordan93@gmail.com>
2023-03-29 17:59:15 -0400

    Createe User Registration View

--- main_api/views.py
+++ main_api/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny
+from .models import User
+from .serializers import MyTokenObtainPairSerializer, RegisterSerializer
 
-# Create your views here.
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = RegisterSerializer
