649dd37688487f35847bc1423f71de2c97a9f8cd
Jordan-Burgess <alfredjordan93@gmail.com>
2023-04-19 14:24:08 -0400

    Create Seeker_Profile model

--- main_api/models.py
+++ main_api/models.py
@@ -13,13 +13,13 @@ class Provider_Profile(models.Model):
     service_type = models.CharField(max_length=500)
     gender_identity = models.CharField(max_length=500)
 
-# class Seeker_Profile(models.Model):
-#     user = models.OneToOneField(User, on_delete=models.CASCADE)
-#     profile_photo = models.CharField(max_length=500)
-#     location = models.CharField(max_length=500)
-#     service = models.CharField(max_length=500)
-#     interests = ArrayField(models.CharField(max_length=500), size=5)
-#     goal = models.CharField(max_length=500)
-#     similar_interest = models.BooleanField()
-#     service_preference = models.CharField(max_length=500)
-#     gender_preference = models.CharField(max_length=500)
+class Seeker_Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = models.CharField(max_length=500)
+    location = models.CharField(max_length=500)
+    service = models.CharField(max_length=500)
+    interests = ArrayField(models.CharField(max_length=500), size=5)
+    goal = models.CharField(max_length=500)
+    similar_interest = models.BooleanField()
+    service_preference = models.CharField(max_length=500)
+    gender_preference = models.CharField(max_length=500)
