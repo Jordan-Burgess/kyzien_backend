4e6713d7aa9dd814e77b8df7c03760f041408d06
Jordan-Burgess <alfredjordan93@gmail.com>
2023-04-19 13:00:14 -0400

    Add additional user fields to registration serializer

--- main_api/serializers.py
+++ main_api/serializers.py
@@ -15,7 +15,7 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('username', 'password', 'password2')
+        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
 
     def validate(self, attrs):
         if attrs['password'] != attrs['password2']:
@@ -23,7 +23,7 @@ class RegisterSerializer(serializers.ModelSerializer):
         return attrs
 
     def create(self, validated_data):
-        user = User.objects.create(username=validated_data['username'])
+        user = User.objects.create(username=validated_data['username'], email=validated_data['email'], first_name=validated_data['first_name'], last_name=validated_data['last_name'])
         user.set_password(validated_data['password'])
         user.save()
         return user
