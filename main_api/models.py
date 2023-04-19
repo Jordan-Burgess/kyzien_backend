dca9cd98ea03b59644f846d69f0c77b85f6c731e
Jordan-Burgess <alfredjordan93@gmail.com>
2023-04-19 14:23:08 -0400

    Change name error of Provider Profile

--- main_api/models.py
+++ main_api/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.postgres.fields import ArrayField
 
-class Seeker_Profile(models.Model):
+class Provider_Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     profile_photo = models.CharField(max_length=500)
     location = models.CharField(max_length=500)
@@ -11,3 +12,14 @@ class Seeker_Profile(models.Model):
     credentials = models.CharField(max_length=500)
     service_type = models.CharField(max_length=500)
     gender_identity = models.CharField(max_length=500)
+
+# class Seeker_Profile(models.Model):
+#     user = models.OneToOneField(User, on_delete=models.CASCADE)
+#     profile_photo = models.CharField(max_length=500)
+#     location = models.CharField(max_length=500)
+#     service = models.CharField(max_length=500)
+#     interests = ArrayField(models.CharField(max_length=500), size=5)
+#     goal = models.CharField(max_length=500)
+#     similar_interest = models.BooleanField()
+#     service_preference = models.CharField(max_length=500)
+#     gender_preference = models.CharField(max_length=500)
