13d7458aa25631fac1bf04cea4bf091fd85f0512
Jordan-Burgess <alfredjordan93@gmail.com>
2023-04-19 14:06:36 -0400

    Create model Seeker_Profile

--- main_api/models.py
+++ main_api/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-# Create your models here.
+
+class Seeker_Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_photo = models.CharField(max_length=500)
+    location = models.CharField(max_length=500)
+    service = models.CharField(max_length=500)
+    specialty = models.CharField(max_length=500)
+    is_licensed = models.BooleanField()
+    credentials = models.CharField(max_length=500)
+    service_type = models.CharField(max_length=500)
+    gender_identity = models.CharField(max_length=500)
