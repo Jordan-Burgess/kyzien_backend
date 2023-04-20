68d05d53ef2ee69550efe30088bc03ce97ff7d57
Jordan-Burgess <alfredjordan93@gmail.com>
2023-04-19 20:20:57 -0400

    Create POST route for Seeker Profile

--- main_api/urls.py
+++ main_api/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('register/', views.RegisterView.as_view(), name='auth_register'),
     path('users/', views.Users.as_view(), name='users'),
     path('token/', views.MyTokenObtainPairView.as_view(), name='token'),
+    path('seeker-onboarding/<int:id>/', views.SeekerProfileView.as_view(), name="seeker_onboarding"),
 ]
--- main_api/views.py
+++ main_api/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework import generics, status
+from rest_framework import generics
 from rest_framework.permissions import AllowAny
 from rest_framework.views import APIView
 from django.http import JsonResponse
 from .models import User
-from .serializers import MyTokenObtainPairSerializer, RegisterSerializer, UserSerializer
+from .serializers import MyTokenObtainPairSerializer, RegisterSerializer, UserSerializer, SeekerProfileSerializer, ProviderProfileSerializer
 
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
@@ -14,6 +14,16 @@ class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     permission_classes = [AllowAny]
     serializer_class = RegisterSerializer
+
+class SeekerProfileView(APIView):
+    def post(self, request, id):
+        request.data['user'] = id
+        serializer = SeekerProfileSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        else:
+            return JsonResponse(serializer.errors)
 
 class Users(APIView):
     def get(self, request):
